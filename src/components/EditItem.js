bd5e91d57fc19a436bdf0e021f6c3bc3c7ea4374
Abhinav <iamabhinav1718@gmail.com>
2022-09-26 01:52:12 +0530

    removed the comments from EditItem.js

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -1,41 +1,29 @@
-import React  , { useState , useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-function EditItem({setIsEditing , setModifiedItem , inputValue}) {
+function EditItem({ setIsEditing, setModifiedItem, inputValue }) {
 
-// console.log('EditItem.js inputValue = ' , inputValue)
-// const [initialValue, setinitialValue] = useState(inputValue)
-const [editValue, setEditValue] = useState(inputValue);
-// const [editValue, setEditValue] = useState('');
+    const [editValue, setEditValue] = useState(inputValue);
 
-// useEffect(() => {
+    function formSubmitHandler(e) {
 
-//     setEditValue(inputValue);
+        e.preventDefault()
+        setIsEditing(false)
+        setModifiedItem(editValue)
+    }
 
-// }, [])
-
-
-function formSubmitHandler(e){
-
-    e.preventDefault()
-    setIsEditing(false)
-    setModifiedItem(editValue)
-}
-
-function inputChangeHandler(e){
-
-    // console.log('inputChangeHandler fired!+++++++++++++++Abhinav Sahu')
-    const val = e.target.value
-    setEditValue(val);
-}
-  return (
-    <form onSubmit={formSubmitHandler}>
-        <input 
-            type="text" 
-            value = {editValue}  
-            onChange={inputChangeHandler}
-        />
-    </form>
-  )
+    function inputChangeHandler(e) {
+        const val = e.target.value
+        setEditValue(val);
+    }
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <input
+                type="text"
+                value={editValue}
+                onChange={inputChangeHandler}
+            />
+        </form>
+    )
 }
 
 export default EditItem
